c2549075eb5f895a5bdd713aa0ab919232ebb77a
Michael "Z" Goddard <mzgoddard@gmail.com>
2011-10-15 16:06:20 -0400

    timer.start(0) async test

--- test/unit/timer.js
+++ test/unit/timer.js
@@ -21,3 +21,23 @@ document.addEventListener( 'DOMContentLoaded', function( e ) {
   }, 400 );
 
 }, false );
+
+module("Abacus.timer");
+
+asyncTest("timer.start(0) calls once", function() {
+  var timesCalled = 0,
+    timer = Abacus.timer({
+      callback: function( timerData ) {
+        timesCalled++;
+        if (timesCalled > 1) {
+          ok(false, "called second time");
+        } else {  
+          ok("callback called");
+          timer.pause();
+          start();
+        }
+      }
+    });
+  
+  timer.start(0);
+});
