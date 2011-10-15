299fd4be95c9adc2fadbfd117948f645c00910e8
secretrobotron <secretrobotron@gmail.com>
2011-10-15 15:28:16 -0400

    Moved timer rAF above callback.

--- src/boilerplate.js
+++ src/boilerplate.js
@@ -31,15 +31,17 @@
         importantStuff.delta = now - _lastTick;
         _lastTick = now;
 				
+				// Check to see if the timer is paused before calling RAF.
+        // Call rAF before update loop to ensure fallback (setTimeout) works with correct timing.
+        if ( !pauseFlag && ( !_until || ( _until && _lastTick - _lastStart < _until ) ) ) {
+          requestAnimFrame(_loop, options.element);
+        }
+
 				// If there is a callback pass the importantStuff to it
 				if( options.callback ) {
           options.callback( importantStuff );
 				}
 
-				// Check to see if the timer is paused before calling RAF
-        if ( !pauseFlag && ( !_until || ( _until && _lastTick - _lastStart < _until ) ) ) {
-          requestAnimFrame(_loop, options.element);
-        }
       }
 
       return {
