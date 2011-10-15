dd11ce40f7979cd4e2e71bce57ba81828ccf3c81
Rick Waldron <waldron.rick@gmail.com>
2011-10-15 19:44:59 -0400

    Lint tweaks and code organization

--- src/boilerplate.js
+++ src/boilerplate.js
@@ -1,106 +1,107 @@
-(function( window ){
+(function( window ) {
+
   var requestAnimFrame = (function(){
     // thanks paul irish
-    return window.requestAnimationFrame || 
-    window.webkitRequestAnimationFrame  || 
-    window.mozRequestAnimationFrame     || 
-    window.oRequestAnimationFrame       || 
-    window.msRequestAnimationFrame      || 
-    function( callback, element ){
-      window.setTimeout( callback, 1000 / 60 );  
-    }
+    return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function( callback, element ) {
+      window.setTimeout( callback, 1000 / 60 );
+    };
   })();
 
-  // An array of callbacks to call in our rAF
-  var _timerCallbacks = [],
+  var Abacus = window.Abacus || {},
+			// An array of callbacks to call in our rAF
+			callbackQueue = [],
       // the function we call on each tick of the rAF
-      _timerLoop = function() {
+      timerLoop = function() {
         // If there are callbacks, then run the loop again
-        if ( _timerCallbacks.length > 0 ) {
-          requestAnimFrame(_timerLoop);
+        if ( callbackQueue.length > 0 ) {
+          requestAnimFrame(timerLoop);
         }
-        _timerLoop.running = _timerCallbacks.length > 0;
-        
-        // Call all the calbacks
-        for( var i=_timerCallbacks.length-1; i>=0; --i ) {
-          _timerCallbacks[ i ]();
+
+        timerLoop.running = callbackQueue.length > 0;
+
+        // Iterate and execute all callbacks in the queue
+        for ( var i = callbackQueue.length-1; i >= 0; --i ) {
+          callbackQueue[ i ]();
         }
-      };
-
-  var Abacus = (function(){
-    var noop = function(){},
-
-    timer = function( options ){
-        // Options is expected to have optional
-        // callback and element properties
-
-      var _lastTick = 0,
-          _lastStart = 0,
-          _until = 0,
-          pauseFlag = false,
-          importantStuff = {
-            delta: 0,
-            
-            // how many times callback is called
-            ticks: 0
-          },
-
-      stop = function() {
-        var idx = _timerCallbacks.indexOf( _loop );
-        _timerCallbacks.splice( idx, 1 );
       },
+			noop = function() {};
 
-      _loop = function() {
-        var now = Date.now();
-        importantStuff.delta = now - _lastTick;
-        _lastTick = now;
-        
-        // Check to see if the timer is paused, or run over until time but ran
-        // at least once
-        if ( pauseFlag || 
-          ( _until != undefined && _lastTick - _lastStart > _until ) &&
-          importantStuff.ticks !== 0 ) 
-        {
-          stop();
-          
-          if( options.complete ){
-            options.complete( importantStuff );
-          }
-        } else {
 
-          // If there is a callback pass the importantStuff to it
-          if( options.callback ) {
-            options.callback( importantStuff );
-          }
-          
-          // zero index, add after call
-          importantStuff.ticks++;
-        }
+	Abacus.timer = function( options ) {
+    // options is expected to have optional
+    // callback and element properties
 
-      };
+		this.isPaused = false;
 
-      return {
-        start: function( until ) {
-          _lastStart = Date.now();
-          _until = until;
-          _lastTick = _lastStart;
-          pauseFlag = false;
 
-          if( !_timerLoop.running ) {
-            requestAnimFrame(_timerLoop);
-            _timerLoop.running = true;
-          }
-          _timerCallbacks.push( _loop );
+    var _lastTick = 0,
+        _lastStart = 0,
+        _until = 0,
+        importantStuff = {
+          delta: 0,
 
+          // how many times callback is called
+          ticks: 0
         },
-        pause: function() {
-          pauseFlag = true;
+
+    stop = function() {
+      var idx = callbackQueue.indexOf( loop );
+      callbackQueue.splice( idx, 1 );
+    },
+
+    loop = function() {
+      var now = Date.now();
+      importantStuff.delta = now - _lastTick;
+      _lastTick = now;
+
+      // Check to see if the timer is paused, or run over until time but ran
+      // at least once
+      if ( isPaused ||
+        ( _until != undefined && _lastTick - _lastStart > _until ) &&
+        importantStuff.ticks !== 0 )
+      {
+        stop();
+
+        if( options.complete ){
+          options.complete( importantStuff );
         }
+      } else {
+
+        // If there is a callback pass the importantStuff to it
+        if( options.callback ) {
+          options.callback( importantStuff );
+        }
+
+        // zero index, add after call
+        importantStuff.ticks++;
       }
+
     };
+
     return {
-      timer: timer
+      start: function( until ) {
+        _lastStart = Date.now();
+        _until = until;
+        _lastTick = _lastStart;
+        isPaused = false;
+
+        if( !timerLoop.running ) {
+          requestAnimFrame(timerLoop);
+          timerLoop.running = true;
+        }
+        callbackQueue.push( loop );
+
+      },
+      pause: function() {
+        this.isPaused = true;
+      }
     }
-  })()
+  };
+
   window.Abacus = Abacus;
 })( window );
