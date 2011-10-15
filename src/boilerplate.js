2e2c7864cc4a5d44068fe05e79809da6214923a1
Boaz Sender <boazsender@Gallus.local>
2011-10-15 17:28:06 -0400

    added complete callback

--- src/boilerplate.js
+++ src/boilerplate.js
@@ -1,15 +1,15 @@
 (function( window ){
-	var requestAnimFrame = (function(){
-		// thanks paul irish
-		return window.requestAnimationFrame || 
-		window.webkitRequestAnimationFrame  || 
-		window.mozRequestAnimationFrame     || 
-		window.oRequestAnimationFrame       || 
-		window.msRequestAnimationFrame      || 
-		function( callback, element ){
-			window.setTimeout( callback, 1000 / 60 );	
-		}
-	})();
+  var requestAnimFrame = (function(){
+    // thanks paul irish
+    return window.requestAnimationFrame || 
+    window.webkitRequestAnimationFrame  || 
+    window.mozRequestAnimationFrame     || 
+    window.oRequestAnimationFrame       || 
+    window.msRequestAnimationFrame      || 
+    function( callback, element ){
+      window.setTimeout( callback, 1000 / 60 );	
+    }
+  })();
 
   var __timerCallbacks = [],
       __timerLoop = function() {
@@ -22,19 +22,19 @@
       };
 
   var Abacus = {
-		noop: function(){},
+    noop: function(){},
 
     timer: function( options ){
-			// Options is expected to have optional
-			// callback and element properties
+      // Options is expected to have optional
+      // callback and element properties
 
       var _lastTick = 0,
           _lastStart = 0,
           _until = 0,
           pauseFlag = false,
-				  importantStuff = {
-				    delta: 0
-				  };
+          importantStuff = {
+            delta: 0
+          };
 
       function stop() {
         var idx = __timerCallbacks.indexOf( _loop );
@@ -46,15 +46,18 @@
         importantStuff.delta = now - _lastTick;
         _lastTick = now;
 				
-				// Check to see if the timer is paused
+        // Check to see if the timer is paused
         if ( pauseFlag || ( _until && _lastTick - _lastStart > _until ) ) {
           stop();
+          if( options.complete ){
+            options.complete();
+          }
         }
 
-				// If there is a callback pass the importantStuff to it
-				if( options.callback ) {
+        // If there is a callback pass the importantStuff to it
+        if( options.callback ) {
           options.callback( importantStuff );
-				}
+        }
 
       }
 
@@ -75,8 +78,7 @@
           pauseFlag = true;
         }
       };
-		}
-	}
+    }
+  }
   window.Abacus = Abacus;
 })( window );
-
