c783cfb3f8ae7c8c48cc15ecd8cc8f50b6ff8774
Rick Waldron <waldron.rick@gmail.com>
2011-10-15 20:27:54 -0400

    Renames boilerplate.js => abacus.timer.js

--- src/abacus.timer.js
+++ src/abacus.timer.js
@@ -46,12 +46,17 @@
     // options is expected to have optional
     // callback and element properties
 
-    this.key = guid();
+    options = options || {};
+
+    // Ensure an id is created for this Timer instance
+    this.id = options.id || guid();
+
+    // Instance tracking properties
     this.lastTick = 0;
     this.lastStart = 0;
     this._until = 0;
     this.isPaused = false;
-    this.importantStuff = {
+    this.timing = {
       delta: 0,
 
       // how many times callback is called
@@ -62,29 +67,30 @@
     this.loop = function() {
       var now = Date.now();
 
-      this.importantStuff.delta = now - this.lastTick;
+      this.timing.delta = now - this.lastTick;
       this.lastTick = now;
 
       // Check to see if the timer is paused, or run over until time but ran
       // at least once
       if ( this.isPaused ||
             ( this._until != null && this.lastTick - this.lastStart > this._until ) &&
-            this.importantStuff.ticks !== 0 ) {
+            this.timing.ticks !== 0 ) {
 
         this.stop();
 
         if ( options.complete ) {
-          options.complete( this.importantStuff );
+          options.complete( this.timing );
         }
       } else {
 
-        // If there is a callback pass the importantStuff to it
+        // If there is a callback pass the timing to it
         if ( options.callback ) {
-          options.callback( this.importantStuff );
+          // Set the callback's context to this Timer instance
+          options.callback( this.timing );
         }
 
         // zero index, add after call
-        this.importantStuff.ticks++;
+        this.timing.ticks++;
       }
 
     }.bind( this );
@@ -123,8 +129,7 @@
     return new Timer( options );
   };
 
-
-
+  // Re-expose Abacus object
   window.Abacus = Abacus;
 
 })( this );
