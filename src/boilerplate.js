1bddb04d2d96d3817c9c6b8fa163defccd8b8a49
Rick Waldron <waldron.rick@gmail.com>
2011-10-15 20:14:53 -0400

    Complete conversion to real constructor; ALL PASSING

--- src/boilerplate.js
+++ src/boilerplate.js
@@ -33,84 +33,97 @@
           callbackQueue[ i ]();
         }
       },
-      noop = function() {};
+      noop = function() {},
+      guid = function() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+          return v.toString(16);
+        }).toUpperCase();
+      };
 
-
-	// Wrap new Timer() construction in Abacus.timer() API
-  Abacus.timer = function( options ) {
+  // Timer constructor (Internal)
+  function Timer( options ) {
     // options is expected to have optional
     // callback and element properties
 
-		return new Timer( options );
-	};
+    this.key = guid();
+    this.lastTick = 0;
+    this.lastStart = 0;
+    this._until = 0;
+    this.isPaused = false;
+    this.importantStuff = {
+      delta: 0,
 
-	// Timer constructor
-	function Timer( options ) {
-    var loop, stop,
-      lastTick = 0,
-      lastStart = 0,
-      _until = 0,
-      isPaused = false,
-      importantStuff = {
-        delta: 0,
+      // how many times callback is called
+      ticks: 0
+    };
 
-        // how many times callback is called
-        ticks: 0
-      };
-
-
-    loop = function() {
+    // Define own property loop() function closure
+    this.loop = function() {
       var now = Date.now();
-      importantStuff.delta = now - lastTick;
-      lastTick = now;
+
+      this.importantStuff.delta = now - this.lastTick;
+      this.lastTick = now;
 
       // Check to see if the timer is paused, or run over until time but ran
       // at least once
-      if ( isPaused ||
-            ( _until != null && lastTick - lastStart > _until ) &&
-            importantStuff.ticks !== 0 ) {
+      if ( this.isPaused ||
+            ( this._until != null && this.lastTick - this.lastStart > this._until ) &&
+            this.importantStuff.ticks !== 0 ) {
 
-        stop();
+        this.stop();
 
         if ( options.complete ) {
-          options.complete( importantStuff );
+          options.complete( this.importantStuff );
         }
       } else {
 
         // If there is a callback pass the importantStuff to it
         if ( options.callback ) {
-          options.callback( importantStuff );
+          options.callback( this.importantStuff );
         }
 
         // zero index, add after call
-        importantStuff.ticks++;
+        this.importantStuff.ticks++;
       }
 
-    };
-
-    stop = function() {
-      callbackQueue.splice( callbackQueue.indexOf( loop || noop ), 1 );
-    };
+    }.bind( this );
 
 
-    return {
-      start: function( until ) {
-        lastStart = Date.now();
-        _until = until;
-        lastTick = lastStart;
-        isPaused = false;
+    // Define own property stop() function closure
+    this.stop = function() {
 
-        if ( !timerLoop.running ) {
-          requestAnimFrame( timerLoop );
-          timerLoop.running = true;
-        }
-        callbackQueue.push( loop );
-      },
-      pause: function() {
-        isPaused = true;
+      callbackQueue.splice( callbackQueue.indexOf( this.loop || noop ), 1 );
+
+    }.bind( this );
+
+    return this;
+  }
+
+  Timer.prototype = {
+    start: function( until ) {
+      this.lastStart = Date.now();
+      this._until = until;
+      this.lastTick = this.lastStart;
+      this.isPaused = false;
+
+      callbackQueue.push( this.loop );
+
+      if ( !timerLoop.running ) {
+        requestAnimFrame( timerLoop );
       }
-    };
+    },
+    pause: function() {
+      this.isPaused = true;
+    }
   };
+
+  // Wrap new Timer() construction in Abacus.timer() API
+  Abacus.timer = function( options ) {
+    return new Timer( options );
+  };
+
+
 
   window.Abacus = Abacus;
 
