23b912ad02f7de6191702feacb17def2ca5a0531
Boaz Sender <boaz@bocoup.com>
2011-10-16 19:39:44 -0400

    getting closer to triggering keydown properly in input test

--- test/unit/abacus.input.js
+++ test/unit/abacus.input.js
@@ -11,3 +11,13 @@ document.addEventListener('keydown', function( e ){
 
 }, false);
 */
+
+/*
+// Trigger a keyboard event, not yet sure how to get it
+// to fire a keyCode:
+
+evt = document.createEvent("KeyboardEvent");
+evt.initEvent('keydown', true, true); // event type,bubbling,cancelable
+document.dispatchEvent(evt);
+
+*/
