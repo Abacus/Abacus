8c4d8c13f1bbfdb6650790b30b1919f516e34602
Boaz Sender <boaz@bocoup.com>
2011-10-16 18:39:08 -0400

    boiler test code in tests, requers human

---
+++ test/unit/abacus.input.js
@@ -0,0 +1,13 @@
+/*
+document.addEventListener('keydown', function( e ){
+
+  // write some code to trigger an event
+
+  document.addEventListener('keydown', function( e ){
+		Abacus.inputs.state( e.keyCode, 'active')
+
+		console.log( Abacus.inputs.state( Abacus.inputsLookup[ e.keyCode ] ) )
+	}, false);
+
+}, false);
+*/
