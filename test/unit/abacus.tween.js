735aa34664266eb416f38892e51a10242bf77815
Andrew Dolce <andrew.dolce@gmail.com>
2011-10-19 13:22:53 -0400

    Removed use of arugments.length from tween.get and added unit test for tween.get() with no args.

--- test/unit/abacus.tween.js
+++ test/unit/abacus.tween.js
@@ -8,7 +8,7 @@ test('Tween instances have step method', 1, function() {
   equal( typeof tween.get, 'function', 'the tween instance has a get method' );
 });
 
-test('Linear Tween is correct', 4, function() {
+test('Linear Tween is correct', 5, function() {
   var tween = Abacus.tween({
     type: "linear",
     start: 6,
@@ -18,7 +18,9 @@ test('Linear Tween is correct', 4, function() {
   equal( tween.get( 0 ), 6, 'get(0) is correct' );
   equal( tween.get( 1 ), 8, 'get(1) is correct' );
   equal( tween.get( .5 ), 7, 'get(.5) is correct' );
+  equal( tween.get(), 7, 'get() is correct' );
   equal( tween.get( 0 ), 6, 'get(0) is still correct after evaluating at a nonzero index' );
+
 });
 
 
