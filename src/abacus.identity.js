4e4f6f63a91caa23923cec100aab10ff30964196
Boaz Sender <boaz@bocoup.com>
2011-11-17 01:19:37 -0500

    implemented achievements

--- src/abacus.identity.js
+++ src/abacus.identity.js
@@ -5,10 +5,17 @@
   function Player( options ){
   
     var _player = Abacus.extend( options, {
-    
-      id: Abacus.guid()
+      id: Abacus.guid(),
+      achievements: [],
+      addAchievement: function( options ){
 
-    
+        options = options || {};
+        
+        var _achievement = new Achievement( options );
+
+        this.achievements.push( _achievement )
+        return this;
+      }
     });
 
     players.push( _player )
@@ -16,11 +23,23 @@
     return _player;
 
   }
+
+  function Achievement( options ){
+  
+    var _achievement = Abacus.extend( options, {
+
+      id: Abacus.guid(),
+      date: Date.now()
+
+    });
+ 
+    return _achievement;
+  }
   
   Abacus.addPlayer = function( options ){
     
     options = options || {};
-
+    
     return new Player( options );
 
   }
@@ -28,9 +47,3 @@
   
 
 })( this, this.Abacus );
-
-/*
-
-Abacus.
-
-*/
