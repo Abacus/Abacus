93043eb0b506d6507f5d9fcca4af9fb67ff0f4a1
Boaz Sender <boaz@bocoup.com>
2011-11-16 21:08:29 -0500

    adding abacus.store.js

--- src/abacus.identity.js
+++ src/abacus.identity.js
@@ -1,5 +1,25 @@
 (function( window, Abacus ) {
 
+  var players = [];
 
+  function Player( options ){
+  
+    this.addAcheivement = function( key, value ){
+      Abacus.store( key, value );
+    }
+
+  }
+  
+  function addPlayer( options ){
+    return new Player( options );
+  }
+
+  
 
 })( this, this.Abacus );
+
+/*
+
+Abacus.
+
+*/
---
+++ src/abacus.store.js
@@ -0,0 +1,5 @@
+(function( window, Abacus ) {
+
+
+
+})( this, this.Abacus );
