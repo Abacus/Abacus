6e57c8e1c584b989e92c58e85dc88afc661ac667
Boaz <boaz@bocoup.com>
2011-11-18 18:42:54 -0500

    mid refactoring of player api -> entity api. this is totally unfinished unstable code.

--- src/abacus.player.js
+++ src/abacus.player.js
@@ -2,7 +2,6 @@
   // Setup a players cache
   // Eventually this should pull from something like
   // Abacus.store.players
-  Abacus.players = [];
 
   // Express the Achievement function which will be used by
   // player.addAchievement to add an achievement to a player
@@ -20,39 +19,73 @@
 
   // Express the Player function which is used by addPlayer
   // to add a player to Abacus
-  function Player( options ) {
-    // Create a temporary variable to hold the player
-    // wich is an extension of arbitrary options passed in and
-    // the props we need to manage players
-    var player = Abacus.extend({
-      id: Abacus.guid(),
-      achievements: [],
-      addAchievement: function( options ) {
+  function Entity() {};
+  Entity.prototype = {
+    // The default name for the JSON `id` attribute is `"id"`. MongoDB and
+    // CouchDB users may want to set this to `"_id"`.
+    idAttribute : 'id',
+    addAchievement: function( options ) {
 
-        options = options || {};
-        
-        // Create a new achievement
-        var achievement = new Achievement( options );
+      options = options || {};
+      
+      // Create a new achievement
+      var achievement = new Achievement( options );
 
-        // Add the achievement to the current player
-        this.achievements.push( achievement );
-        // Return the current player
-        return this;
-      }
-    }, options);
-
-    // Push the player onto Abacus.players
-    Abacus.players.push( player );
-
-    // Return the player
-    return player;
+      // Add the achievement to the current entity
+      this.attributes.achievements.push( achievement );
+      // Return the current entity
+      return this;
+    },
+    // Serialize the entity
+    toJSON : function() {
+      return JSON.stringify( this.attributes );
+    },
+    // Get the value of an entity's property.
+    get : function( prop ) {
+      return this.attributes[prop];
+    },
+    // Returns `true` if the property contains a value that is not null
+    // or undefined.
+    has : function( prop ) {
+      return this.attributes[prop] != null;
+    },
+    // Set a property on an entity's attributes
+    set : function( prop ) {
+      this.attributes[prop] = prop;
+      return this;
+    },
+    // Remove a property from the entity
+    unset : function( prop ) {
+      delete this.attributes[prop];
+      return this;
+    }
   }
 
   // Nice sugar to create a new player
-  Abacus.player = {
-    add: function( options ) {
+  Abacus.entity = {
+    entities: [],
+    create: function( options ) {
       options = options || {};
-      return new Player( options );
-    }
+      var entity = new Entity();
+      
+      entity.attributes = Abacus.extend({
+        id: Abacus.guid(),
+        cid: '',
+        type: 'player',
+        achievements: [],
+      }, options);
+      
+      return entity;
+    },
+    get: function( id ) {
+      
+    },
+    set: function( id ) {
+      
+    },
+    delete: function( id ) {
+      
+    }    
   };
 })( this, this.Abacus );
+
--- test/unit/abacus.player.js
+++ test/unit/abacus.player.js
@@ -1,28 +1,28 @@
 module('Animation');
 
 test('identity api exists', 2, function() {
-  ok( Abacus.player.add, 'addPlayer exists' );
-  ok( Abacus.players, 'players exists' );
+  ok( Abacus.entity.create, 'addPlayer exists' );
+  ok( Abacus.entity, 'players exists' );
 });
 
 test('addPlayer constructs a player correctly', 4, function() {
-  var boaz = Abacus.player.add({name: 'boaz'});
+  var boaz = Abacus.entity.create({name: 'boaz'});
 
-  equal( typeof boaz.achievements, 'object', 'achievements exists on player and is an object' )
+  equal( typeof boaz.attributes.achievements, 'object', 'achievements exists on player and is an object' )
   equal( typeof boaz.addAchievement, 'function' , 'addAchievements exists on player and is a function' )
-  equal( boaz.id.length, 36, 'id exists on player and is 36 characters long' )
-  ok( boaz.name, 'the name property was properly merged onto the player object' )
+  equal( boaz.attributes.id.length, 36, 'id exists on player and is 36 characters long' )
+  ok( boaz.attributes.name, 'the name property was properly merged onto the player object' )
 });
 
 test('addAcheivemnt constructs an acheivement properly and stores it on a player', 4, function() {
-  var boaz = Abacus.player.add({name: 'boaz'});                                    
+  var boaz = Abacus.entity.create({name: 'boaz'});                                    
 
   boaz.addAchievement({ points: 10 })
 
-  ok( boaz.achievements[0], 'achievement exist' )
-  ok( boaz.achievements[0].date, 'achievement has date property' )
-  equal( boaz.achievements[0].id.length, 36, 'achievement has an id property that is 36 characters long' )
-  equal( boaz.achievements[0].points, 10, 'achievement has points property that has been properddly merged on' )
+  ok( boaz.attributes.achievements[0], 'achievement exist' )
+  ok( boaz.attributes.achievements[0].date, 'achievement has date property' )
+  equal( boaz.attributes.achievements[0].id.length, 36, 'achievement has an id property that is 36 characters long' )
+  equal( boaz.attributes.achievements[0].points, 10, 'achievement has points property that has been properddly merged on' )
  
 });
 
