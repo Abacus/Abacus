f66377f08f68f1c333ad47104154c2f252f8251f
Boaz Sender <boaz@bocoup.com>
2011-11-19 01:27:10 -0500

    more tests for entity

--- test/unit/abacus.entity.js
+++ test/unit/abacus.entity.js
@@ -6,16 +6,25 @@ test('identity api exists', 6, function() {
   ok( Abacus.entity.create, 'entity.create exists' );
   ok( Abacus.entity.get, 'entity.get exists' );
   ok( Abacus.entity.set, 'entity.set exists' );
-  ok( Abacus.entity.delete, 'entity.delete exists' );
+  ok( Abacus.entity.destroy, 'entity.destroy exists' );
 });
 
-test('Abacus.entity.create constructs a player correctly', 4, function() {
+test('Abacus.entity.create constructs a player correctly', 9, function() {
   var boaz = Abacus.entity.create({name: 'boaz'});
 
   equal( typeof boaz.attributes.achievements, 'object', 'achievements exists on player and is an object' )
   equal( typeof boaz.addAchievement, 'function' , 'addAchievements exists on player and is a function' )
+  equal( typeof boaz.get, 'function' , 'get exists on player and is a function' )
+  equal( typeof boaz.has, 'function' , 'has exists on player and is a function' )
+  equal( typeof boaz.set, 'function' , 'set exists on player and is a function' )
+  equal( typeof boaz.toJSON, 'function' , 'toJSON exists on player and is a function' )
+  equal( typeof boaz.unset, 'function' , 'unset exists on player and is a function' )
   equal( boaz.attributes.id.length, 36, 'id exists on player and is 36 characters long' )
   ok( boaz.attributes.name, 'the name property was properly merged onto the player object' )
+  
+  // boaz.get('name')
+  // boaz.get('id').length
+  // boaz.has('name')
 });
 
 test('addAcheivemnt constructs an acheivement properly and stores it on a player', 4, function() {
