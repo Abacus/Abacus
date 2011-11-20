4d7a274f5cdc0abf16bacd5edc40869e0af29115
Boaz Sender <boaz@bocoup.com>
2011-11-20 17:48:00 -0500

    introducing unit tests for Abacus.store re: issue #67

--- src/abacus.store.js
+++ src/abacus.store.js
@@ -1,17 +1,29 @@
 (function( window, Abacus ) {
+  
+  var abacusCache = {};
 
   Abacus.store = {
-    get: function( id ){
-      
+    create: function( id, value ){
+      abacusCache[ id ] = value;
+      window.localStorage.setItem( 'Abacus', JSON.stringify( abacusCache ) );
+      return this;
     },
-    set: function( id ){
-      
+    read: function( id ){
+      var abacusCache = JSON.parse( window.localStorage.getItem( 'Abacus') );      
+      return abacusCache[ id ];
+    },
+    update: function( id, value ){
+      Abacus.extend( abacusCache[ id ], value );
+      window.localStorage.setItem( 'Abacus', JSON.stringify( abacusCache ) );      
+      return this;
     },
     destroy: function( id ){
-      
+      delete abacusCache[ id ];
+      window.localStorage.setItem( 'Abacus', JSON.stringify( abacusCache ) );
+      return this;
     },
-    update: function( id ){
-      
+    readAll: function() {
+      return JSON.parse( window.localStorage.getItem( 'Abacus') );      
     }
   };
 
--- test/unit/abacus.store.js
+++ test/unit/abacus.store.js
@@ -1,4 +1,19 @@
 module('Store');
-test('Test that the Abacus.store exists', 1, function() {
+test('Test that the Abacus.store and its methods exists', 6, function() {
   ok( Abacus.store, 'Abacus.store exists' );
+  ok( Abacus.store.create, 'Abacus.store.create exists' );
+  ok( Abacus.store.read, 'Abacus.store.read exists' );
+  ok( Abacus.store.update, 'Abacus.store.update exists' );
+  ok( Abacus.store.destroy, 'Abacus.store.destroy exists' );
+  ok( Abacus.store.readAll, 'Abacus.store.readAll exists' );
+});
+
+test('Test the Abacus.store methods', 4, function() {
+
+Abacus.store.create( 'asd', {name: 'boaz'} )
+
+  equal( Abacus.store.read( 'asd' ).name, 'boaz', 'Abacus.store.read returns a property from the target object correctly' );
+  equal( Abacus.store.update( 'asd', { age: 25 } ).read('asd').age, 25, 'Abacus.store.update adds a property to the target object correctly' );
+  equal( Abacus.store.destroy( 'asd' ).read( 'asd' ), undefined , 'Abacus.store.destroy removes the target object from the cache' );
+  equal( typeof Abacus.store.readAll(), 'object', 'Abacus.store.readAll returns an object' );
 });
