cf3c2c0ae0ff8d7884f48c9a26953096ef9447e5
Boaz Sender <boaz@bocoup.com>
2011-11-22 22:56:45 -0500

    intital events implementation, trigger wont work, neads Abacus.forEach re #68

--- src/abacus.event.js
+++ src/abacus.event.js
@@ -1,10 +1,38 @@
-(function( window, Abacus ) {
+(function ( window, Abacus ) {
   Abacus.event = {
-    bind: function( name, callback, context ) {
+    _callbacks: {},
+    bind: function ( name, callback ) {
+      if ( !this._callbacks[ name ]  ) {
+        this._callbacks[ name ] = [];
+      }
+      this._callbacks[ name ].push ( callback  ) ;
+      return this;
     },
-    unbind: function( name, callback, callback) {
+    unbind: function ( name, callback ) {
+      var calls;
+      if ( !name ) {
+        // If no name is passed, unbind all callbacks
+        this._callbacks = {};
+      } else if ( !callback ) {
+        calls[ name ] = [];
+      } else {
+        var callbacks = _callbacks[ name ];
+        for (var i = 0, c = callbacks.length; i < c; i++ ) {
+          if ( callbacks[ i ] && callback === callbacks[ i ][ 0 ] ) {
+            callbacks[ i ] = null;
+            break;
+          }
+        }
+      }
+      return this;
     },
-    trigger: function(eventName) {
+    trigger: function ( name ) {
+      if ( this._callbacks[ name ] ) {
+        Abacus.forEach( this._callbacks[ name ], function ( ) {
+          this.apply(d, args || [] ) ;
+        });
+      }
+      return this;
     }
   };
-})( this, this.Abacus );
+})( this, this.Abacus  );
