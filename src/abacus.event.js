12a0c6c82789a78f32550e21a17ed00c16214ac6
Boaz Sender <boaz@bocoup.com>
2011-11-22 23:57:17 -0500

    adding more comments

--- src/abacus.event.js
+++ src/abacus.event.js
@@ -1,6 +1,12 @@
 (function ( window, Abacus ) {
+  // Use Abacus.event to dispatch events globally
+  // or mix Abacus.event in with Abacus.extend({}, Abacus.event)
+  // to use the event system locally on a given oject
   Abacus.event = {
+    // Local scoped cache of callbacks
     _callbacks: {},
+    
+    // Bind a callback to a name for the current scope
     bind: function ( name, callback ) {
       if ( !this._callbacks[ name ]  ) {
         this._callbacks[ name ] = [];
@@ -8,14 +14,22 @@
       this._callbacks[ name ].push ( callback  ) ;
       return this;
     },
+    
+    // Unbind events from the current scope
+    // three signatures supported:
+    // .unbind(), .unbind( name ), .unbind( name, callback )
     unbind: function ( name, callback ) {
+      // If no name is passed, unbind all callbacks
       if ( !name ) {
-        // If no name is passed, unbind all callbacks
         this._callbacks = {};
+
+      // If a name is passed, but no callback,
+      // unbind all callbacks for that name
       } else if ( !callback ) {
-        // If a name is passed, but no callback,
-        // unbind all callbacks for that name
         this._callbacks[ name ] = [];
+
+      // Otherwise run through all the callbacks for that name
+      // and unbind the right one
       } else {
         var callbacks = _callbacks[ name ];
         for (var i = 0, c = callbacks.length; i < c; i++ ) {
@@ -27,6 +41,8 @@
       }
       return this;
     },
+    
+    // Trigger an event on the current scope
     trigger: function ( name ) {
       if ( this._callbacks[ name ] ) {
         this._callbacks[ name ].forEach(function( element, index, array ) {
